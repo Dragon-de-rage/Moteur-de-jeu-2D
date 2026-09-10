8742abe069f7d6baf14b3414e37825f83ffb35e5
elboscchar <Elven.BOSC--CHARLES@etu.uca.fr>
2026-09-10 15:44:28 +0200

    Affichage des axes en mode debug

--- template_jeu_sfml/GameWindow.cpp
+++ template_jeu_sfml/GameWindow.cpp
@@ -52,6 +52,31 @@ void GameWindow::render()
     // Clear background with White color.
     _window.clear(sf::Color::White);
 
+#ifdef SFML_DEBUG // if in debug, prints the axis
+    sf::Vector2u screen_res = _window.getSize();
+    float y = screen_res.y; // because screen_res.y is an uint
+    sf::RectangleShape axe_x(sf::Vector2f(screen_res.x, 1));
+    sf::RectangleShape axe_y(sf::Vector2f(1, -y));
+    axe_x.setFillColor(sf::Color::Red);
+    axe_y.setFillColor(sf::Color::Red);
+    axe_x.setPosition(sf::Vector2f(screen_res.x/2, screen_res.y/2));
+    axe_y.setPosition(sf::Vector2f(screen_res.x/2, screen_res.y/2));
+    _window.draw(axe_x);
+    _window.draw(axe_y);
+    // draws graduations each 10 pixels
+    sf::RectangleShape graduation(sf::Vector2f(1, 10));
+    graduation.setFillColor(sf::Color::Red);
+    for (size_t i = 10; i < screen_res.x/2; i+=10){ // on x axis
+        graduation.setPosition(sf::Vector2f(screen_res.x/2 + i, screen_res.y/2 - 5));
+        _window.draw(graduation);
+    }
+    graduation.setSize(sf::Vector2f(10, 1));
+    for (size_t i = 10; i < screen_res.y/2; i+=10){ // on x axis
+        graduation.setPosition(sf::Vector2f(screen_res.x/2 - 5, screen_res.y/2 - i));
+        _window.draw(graduation);
+    }
+#endif
+
     // Draw player as a yellow circle.
     int radius = 10;
 
