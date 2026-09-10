4b580089e636272cf7102dcf76a237686f32c193
Alizee Chambon <chambon.alizee@yahoo.com>
2026-09-10 15:07:44 +0200

    ajout 1.2 physique -> ecran

--- template_jeu_sfml/main.cpp
+++ template_jeu_sfml/main.cpp
@@ -1,5 +1,18 @@
 #include <GameWindow.h>
 
+
+
+using ScreenPoint = std::pair<int, int>;
+using WorldPoint = std::pair<double, double>;
+
+ScreenPoint toScreen(const WorldPoint& point, int W, int H, double z)
+{
+    int X = W / 2 + point.first * z;
+    int Y = H / 2 - point.second * z;
+
+    return { X, Y };
+}
+
 int main()
 {
     GameWindow window;
